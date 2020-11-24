88189c936dae0fd64bdc0e2983455041da31e74f
VioletZhouECE <x375zhou@uwaterloo.ca>
2020-11-23 22:55:32 -0500

    add another partition function

--- sort/src/quick_sort.java
+++ sort/src/quick_sort.java
@@ -1,4 +1,10 @@
 class QuickSort {
+    /*
+    * Notes:
+    * 1. Use of pre-increment makes sure that the final lo and hi take on the values that break the while condition
+    * 2. We need to consider the special cases where i or j traverse all the way through the array
+    * 3. The break condition is if (hi <= lo). Not if (hi < lo)
+    * */
     public static int partition(int start, int end, int[] array){
         if (start==end) return start;
 
@@ -26,6 +32,30 @@ class QuickSort {
         return hi;
     }
 
+    /* Easier to implement but results in more exchanges
+    * Notes:
+    * 1. we need to check lo == hi (the last item needs to be examined), hence the while condition: while(lo<=hi)
+    * 2. In this approach we "focus on" moving one pointer - i, so it's easier to think through :)
+    * */
+    public static int partition_easy(int start, int end, int[] array){
+        if (start==end) return start;
+
+        // choose the first item as the pivot point
+        int pivot = array[start];
+        int lo = start;
+        int hi = end;
+
+        while(lo<=hi){
+            if (array[lo] <= pivot) lo++;
+            else exch(lo, hi--, array);
+        }
+
+        //place the pivot in the correct position of the array by exchanging it with hi
+        exch(start, hi, array);
+
+        return hi;
+    }
+
     private static void exch(int x, int y, int[] array){
         int x_val = array[x];
         array[x] = array[y];
@@ -42,9 +72,10 @@ class QuickSort {
 
     public static void main(String[] args){
         int[] test_array1 = {2,3,4,0,7,2};
+        int[] test_array2 = {2,2,2,2,1,1};
         //partition(0, test_array1.length-1, test_array1);
-        quicksort(0, test_array1.length-1, test_array1);
-        for (int num : test_array1){
+        quicksort(0, test_array2.length-1, test_array2);
+        for (int num : test_array2){
             System.out.println(num);
         }
     }
