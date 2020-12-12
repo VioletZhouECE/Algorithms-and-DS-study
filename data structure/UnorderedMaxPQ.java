33775634724981d9c1e477fe2fea34cbfaa9fc01
VioletZhouECE <x375zhou@uwaterloo.ca>
2020-12-12 15:00:20 -0500

    UnorderedMaxPQ API

---
+++ data structure/UnorderedMaxPQ.java
@@ -0,0 +1,19 @@
+class UnorderedMaxPQ<Key extends Comparable<Key>>{
+    public UnorderedMaxPQ(int capacity){
+    }
+
+    public boolean isEmpty(){
+        return true;
+    }
+
+    public void insert(Key x){
+    }
+
+    public Key delMax(){
+        return null;
+    }
+
+    public static void main(String[] args){
+        //test code goes here
+    }
+}
